24c7765a9817a247871fb3d92f171f525e173fb6
fahsfahg <fahsfahg@gmail.com>
2023-04-14 22:26:30 +0700

    fah update

--- SERVER/server.test.js
+++ SERVER/server.test.js
@@ -16,8 +16,8 @@ describe("GET /login", () => {
   });
 });
 
-test("Querry", async () => {
-    const response = await request(app).get("/question");
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("[{\"_id\":\"6432e4327053bbf8b5ac2620\",\"id\":1,\"title\":\"Bisection\",\"question\":\"(x^4)-13\"},{\"_id\":\"6432e4d57053bbf8b5ac2621\",\"id\":2,\"title\":\"One-Point\",\"question\":\"(x^2)-7\"},{\"_id\":\"64339dfb12b1ca9efb7fe304\",\"id\":4,\"title\":\"Bisection\",\"question\":\"(x^2)-7\"},{\"_id\":\"6433cdc332a57b0caab376ff\",\"id\":3,\"title\":\"Newton-Raphson\",\"question\":\"x^4\"}]");
-  });
+// test("Querry", async () => {
+//     const response = await request(app).get("/question");
+//     expect(response.statusCode).toBe(200);
+//     expect(response.text).toBe("[{\"_id\":\"6432e4327053bbf8b5ac2620\",\"id\":1,\"title\":\"Bisection\",\"question\":\"(x^4)-13\"},{\"_id\":\"6432e4d57053bbf8b5ac2621\",\"id\":2,\"title\":\"One-Point\",\"question\":\"(x^2)-7\"},{\"_id\":\"64339dfb12b1ca9efb7fe304\",\"id\":4,\"title\":\"Bisection\",\"question\":\"(x^2)-7\"},{\"_id\":\"6433cdc332a57b0caab376ff\",\"id\":3,\"title\":\"Newton-Raphson\",\"question\":\"x^4\"}]");
+//   });
